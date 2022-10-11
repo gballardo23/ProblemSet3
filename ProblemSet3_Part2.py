9b8d94c153f9ac4801867974b878faceefd04c5a
gballardo23 <gabriella.ballardo@duke.edu>
2022-10-11 13:29:23 -0400

    Moved csv files and completed 4.1

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -4,13 +4,13 @@
 fileObj = open(file='transshipment_vessels_20180723.csv',mode='r')
 
 #Read the entire contents into a list object
-lineList = fileObj.█()
+lineList = fileObj.readlines()
 
 #Release the link to the file objects (now that we have all its contents)
-fileObj.█() #Close the file
+fileObj.close() #Close the file
 
 #Save the contents of the first line in the list of lines to the variable "headerLineString"
-headerLineString = █
+headerLineString = lineList[0]
 
 #Print the contents of the headerLine
-print(█)
+print(headerLineString)
